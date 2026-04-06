365e8bb388e05c82577cb164eb077c900dceee76
0xABADCAFE <karlchurchill@gmail.com>
2026-04-06 16:48:13 +0100

    Added loadImage() to SimpleMemory to allow for a cleaner main.

--- src/include/bus/memory.hpp
+++ src/include/bus/memory.hpp
@@ -3,6 +3,7 @@
 #	include "concepts.hpp"
 #	include "constants.hpp"
 #	include <cstring>
+#   include <cstdio>
 
 namespace C6502PP::Bus {
 
@@ -24,6 +25,20 @@ struct SimpleMemory {
     SimpleMemory& hardReset() noexcept {
         std::memset(bytes, 0, sizeof(bytes));
         return *this;
+    }
+
+    /**
+     * Load an image into memory at the given address. Attempts to load at most MEM_SIZE data.
+     * Returns total bytes read.
+     */
+    size_t loadImage(char const* sImage, Address iLocation = 0) {
+        size_t iResult = 0;
+        std::FILE* pImage = std::fopen(sImage, "rb");
+        if (pImage) {
+            iResult = std::fread(bytes, 1, (MEM_SIZE - iLocation), pImage);
+            std::fclose(pImage);
+        }
+        return iResult;
     }
 };
 
